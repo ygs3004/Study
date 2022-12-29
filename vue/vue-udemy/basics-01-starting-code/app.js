6b372354120fb96ae9178c02b0902843cdc6bed5
윤건수 <ygs3004@naver.com>
2022-12-30 02:19:19 +0900

    221230_studying_vue_udemy

--- vue/vue-udemy/basics-01-starting-code/app.js
+++ vue/vue-udemy/basics-01-starting-code/app.js
@@ -1,8 +1,20 @@
 const app = Vue.createApp({
-    data(){
+    data() {
         return {
-          courseGoal: 'Finish the course and learn Vue!'
+            courseGoalA: 'Finish the course and learn Vue!',
+            courseGoalB: '<h2>Master Vue and build amazing apps!<h2>',
+            vueLink: 'https://vuejs.org'
         };
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return this.courseGoalA;
+            } else {
+                return this.courseGoalB;
+            }
+        }
     }
 });
 
