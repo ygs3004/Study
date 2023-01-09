298385153f19f5f720c90b37130da82af5410c83
윤건수 <ygs3004@naver.com>
2023-01-09 23:13:38 +0900

    230109_vue_monster_special_attack

--- vue-udemy/07-prj-monster/app.js
+++ vue-udemy/07-prj-monster/app.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             monsterHealth: 100,
+            currentRound: 0
         }
     },
     computed: {
@@ -15,10 +16,15 @@ const app = Vue.createApp({
       },
         playerBarStyle(){
           return {width: this.playerHealth + '%'};
+        },
+        mayUseSpecialAttack() {
+            return this.currentRound % 3 !== 0
         }
     },
+
     methods: {
         attackMonster(){
+            this.currentRound++
             const attackValue = getRandomValue(5, 12);
             this.monsterHealth -= attackValue;
             this.attackPlayer();
@@ -26,6 +32,12 @@ const app = Vue.createApp({
         attackPlayer(){
             const attackValue = getRandomValue(8, 15);
             this.playerHealth -= attackValue;
+        },
+        specialAttackMonster() {
+            this.currentRound++
+            const attackValue = getRandomValue(10, 25);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
         }
     }
 });
