45e6c5091b555e806f1768e9d887565477c94340
윤건수 <ygs3004@naver.com>
2023-01-26 00:53:03 +0900

    230126 유데미 뷰인강 86섹션까지

--- vue-udemy/11-vue-cli-01-a-new-vue-project/src/App.vue
+++ vue-udemy/11-vue-cli-01-a-new-vue-project/src/App.vue
@@ -1,10 +1,32 @@
 <template>
-
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
 </template>
 
 <script>
 export default {
-  name: "App"
+  data() {
+    return{
+      friends: [
+        {
+          id: 'manual',
+          name: 'Manuel Lorenz',
+          phone: '010 1234 6789',
+          email: 'manuel@localhost.com'
+        },
+        {
+          id: 'julie',
+          name: 'Julie Jones',
+          phone: '010 2345 2929',
+          email: 'julie@localhost.com'
+        }
+      ]
+    }
+  },
 }
 </script>
 
