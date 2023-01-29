22dae0cc44e0374ba1931a3dec74d0805556d3c8
윤건수 <ygs3004@naver.com>
2023-01-30 00:10:26 +0900

    230130 유데미 vue 컴포넌트 추가

--- vue-udemy/11-vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
+++ vue-udemy/11-vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -1,11 +1,33 @@
 <template>
-
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetail">Show Details</button>
+    <ul v-if="detailAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
 </template>
 
 <script>
 export default {
-  name: "FriendContact"
-}
+  data() {
+    return {
+      detailAreVisible: false,
+      friend: {
+        id: 'manual',
+        name: 'Manuel Lorenz',
+        phone: '010 1234 6789',
+        email: 'manuel@localhost.com'
+      },
+    }
+  },
+  methods: {
+    toggleDetail() {
+      this.detailAreVisible = !this.detailAreVisible;
+    },
+  }
+};
 </script>
 
 <style scoped>
