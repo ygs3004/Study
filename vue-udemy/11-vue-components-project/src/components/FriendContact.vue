654655725cf901c1231466f02b75be092ab6487c
윤건수 <ygs3004@naver.com>
2023-02-04 20:47:48 +0900

    230204 유데미 vue 96: 프로퍼티 값

--- vue-udemy/11-vue-components-project/src/components/FriendContact.vue
+++ vue-udemy/11-vue-components-project/src/components/FriendContact.vue
@@ -19,12 +19,30 @@
 <script>
 export default {
   /* 컴포넌트에서 props 변수명은 카멜케이스 사용(this를 이용하여 호출할때 '-'를 변수로 쓸수없음) */
-  props: [
-    'name',
-    'phoneNumber',
-    'emailAddress',
-    'isFavorite'
-  ],
+  // props: ['name','phoneNumber','emailAddress','isFavorite'],
+  props: {
+    name: {
+      type: String, // String이 아닐경우 에러
+      // 지원되는 프로퍼티 값 : String, Number, Boolean, Array, Object, Date, Function, Symbol
+      required: true // 반드시 프로퍼티가 값이 있어야한다는 의미
+    },
+    phoneNumber:{
+      type: String,
+      required: true
+    },
+    email:{
+      type: String,
+      required: true
+    },
+    isFavorite:{
+      type: String,
+      required: false,
+      default: '0', // 기본값 설정 가능(함수로도 가능 function(){})
+      validator: function (value) { // props에 입력된 값이 유효한지 확인
+        return value === '1' || value === '0'; // 1또는 0이면 true, 아니면 false를 반환
+      },
+    },
+  },
   data() {
     return {
       detailAreVisible: false,
