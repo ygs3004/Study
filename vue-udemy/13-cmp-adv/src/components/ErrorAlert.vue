71c7c49c4aa5939decb266c0b1cc7763367d00ed
윤건수 <ygs3004@naver.com>
2023-05-27 18:31:07 +0900

    [2023/05/27] udemy - Vue 120강: 이론 적용하기(모달창 만들기)

---
+++ vue-udemy/13-cmp-adv/src/components/ErrorAlert.vue
@@ -0,0 +1,23 @@
+<template>
+  <dialog open>
+    <slot></slot>
+  </dialog>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+  dialog{
+    margin:0;
+    position: fixed;
+    top: 20vh;
+    left: 30vw;
+    width: 40vw;
+    background-color:white;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    padding: 1rem;
+  }
+</style>
--- vue-udemy/13-cmp-adv/src/components/ManageGoals.vue
+++ vue-udemy/13-cmp-adv/src/components/ManageGoals.vue
@@ -1,4 +1,35 @@
 <template>
   <h2>ManageGoals</h2>
-  <input type="text"/>
+  <input type="text" ref="goal"/>
+  <button @click="setGoal">SetGoal</button>
+  <error-alert v-if="inputIsValid">
+    <h2>입력값이 존재하지 않습니다.</h2>
+    <p>값을 입력해주세요.</p>
+    <button @click="confirmError">닫기</button>
+  </error-alert>
 </template>
+
+<script>
+import ErrorAlert from "@/components/ErrorAlert.vue";
+export default{
+  components: {
+    ErrorAlert
+  },
+  data(){
+    return{
+      inputIsValid: false,
+    }
+  },
+  methods:{
+    setGoal(){
+      const enteredValue = this.$refs.goal.value;
+      if(enteredValue === ''){
+        this.inputIsValid = true;
+      }
+    },
+    confirmError(){
+      this.inputIsValid = false;
+    }
+  }
+}
+</script>
