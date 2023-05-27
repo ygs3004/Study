ef13360a67df06b0a4ed5fd382f8a39394c5c20b
윤건수 <ygs3004@naver.com>
2023-05-28 00:56:52 +0900

    [2023/05/28] udemy - Vue 129강  - 섹션10: 학습 리소스앱 만들기   > 129강: 초기 컴포넌트 및 프로퍼티 설정

--- vue-udemy/14-prj-cmp/src/App.vue
+++ vue-udemy/14-prj-cmp/src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <ul>
-    <li v-for='resource in storedResources' :key=resource.id>
-
-    </li>
+    <learning-resource v-for='res in storedResources'
+                       :key='res.id'
+                       :title='res.title'
+                       :description='res.description'
+                       :link='res.link'>
+    </learning-resource>
   </ul>
 </template>
 
 <script>
+import LearningResource from '@/components/learning-resources/LearningResource.vue';
 export default {
+  components: {
+    LearningResource
+  },
   data(){
     return {
       storedResources: [
