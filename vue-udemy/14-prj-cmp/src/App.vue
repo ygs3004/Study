208f5938d105e8913bf76b0fcda269598420932d
윤건수 <ygs3004@naver.com>
2023-05-28 00:34:38 +0900

    [2023/05/28] udemy - Vue 127, 128 - 127강: 섹션 소개 - 128강: 설정 및 초기단계

---
+++ vue-udemy/14-prj-cmp/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <li v-for='resource in storedResources' :key=resource.id>
+
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      storedResources: [
+        {
+          id:'공식가이드',
+          title: '공식가이드',
+          description: 'Vue.js 공식 가이드',
+          link: 'https://vuejs.org',
+        },
+        {
+          id:'구글',
+          title: '구글',
+          description: '구글배우기',
+          link: 'https://google.org',
+        }
+      ]
+    }
+  }
+};
+</script>
+<style>
+</style>
