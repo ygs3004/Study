1a8c41cfcd43515ec1f6ea30ffad1014fa21e05a
윤건수 <ygs3004@naver.com>
2023-05-29 00:11:13 +0900

    [2023/05/28] udemy - Vue 144강 - 섹션11: 양식(Forms)   > 144강: v-model 및 드롭다운

--- vue-udemy/15-forms/src/components/TheForm.vue
+++ vue-udemy/15-forms/src/components/TheForm.vue
@@ -10,7 +10,7 @@
     </div>
     <div class='form-control'>
       <label for='referrer'>How did you hear about us?</label>
-      <select id='referrer' name='referrer'>
+      <select id='referrer' name='referrer' v-model='referrer'>
         <option value='google'>Google</option>
         <option value='wom'>Word of mouth</option>
         <option value='newspaper'>Newspaper</option>
@@ -57,7 +57,8 @@ export default {
   data() {
     return {
       userName: '',
-      userAge: null
+      userAge: null,
+      referrer: 'wom'
     };
   },
   methods: {
@@ -68,6 +69,9 @@ export default {
       console.log(this.userAge + 5);
       console.log(31);
       console.log(this.$refs.ageInput.value + 5);
+      this.userAge = null;
+      console.log(this.referrer);
+      this.referrer = 'wom';
     }
   }
 };
