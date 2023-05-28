2528d4241bd44cf65d63c876dc3153c67cbfa021
윤건수 <ygs3004@naver.com>
2023-05-28 17:50:54 +0900

    [2023/05/28] udemy - Vue 135강 - 섹션10: 학습 리소스앱 만들기   > 135강: 양식 추가하기

--- vue-udemy/14-prj-cmp/src/components/learning-resources/AddResource.vue
+++ vue-udemy/14-prj-cmp/src/components/learning-resources/AddResource.vue
@@ -1,12 +1,44 @@
 <template>
-  <h2>Add Resource</h2>
+  <base-card>
+    <form>
+      <div class="form-control">
+        <label for="title">Title</label>
+        <input id="title" name="title" type="text">
+      </div>
+      <div class="form-control">
+        <label for="description">Title</label>
+        <textarea id="description" name="description" rows="3"></textarea>
+      </div>
+      <div class="form-control">
+        <label for="link">Link</label>
+        <input id="link" name="link" type="url">
+      </div>
+      <div>
+        <base-button type="submit">Add Resource</base-button>
+      </div>
+    </form>
+  </base-card>
 </template>
 
-<script>
-export default {
-}
-</script>
-
 <style scoped>
-
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+input, textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+input:focus, textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+.form-control {
+  margin: 1rem 0;
+}
 </style>
