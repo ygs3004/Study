39324384d393b7f4d0396684c9607a63178aec32
윤건수 <ygs3004@naver.com>
2023-05-29 20:06:18 +0900

    [2023/05/29] udemy - Vue 169강  - 섹션13: 라우팅(Routing): "다중 페이지"가 있는 SPA 구축하기   > 169강: router-link로 탐색하기

--- vue-udemy/17-routing/src/main.js
+++ vue-udemy/17-routing/src/main.js
@@ -2,12 +2,18 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
-
-const app = createApp(App);
+import TeamsList from '@/components/teams/TeamsList.vue';
+import UsersList from '@/components/users/UsersList.vue';
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [],
+  routes: [
+    { path: '/teams', component: TeamsList },
+    { path: '/users', component: UsersList },
+  ], // out-domain.com/teams
 });
 
+const app = createApp(App);
+
+app.use(router);
 app.mount('#app');
