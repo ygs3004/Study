29aaaee902e5d7d920ae8469e0b6e1cb693f7681
윤건수 <ygs3004@naver.com>
2023-05-29 23:41:01 +0900

    [2023/05/29] udemy - Vue 178강  - 섹션13: 라우팅(Routing): "다중 페이지"가 있는 SPA 구축하기   > 178강: 리디렉션 및 "Catch All" 라우트

---
+++ vue-udemy/17-routing/src/components/nav/NotFound.vue
@@ -0,0 +1,4 @@
+<template>
+  <h2>Page not found! Maybe view our</h2>
+  <router-link to='/teams'>Teams</router-link>
+</template>
--- vue-udemy/17-routing/src/main.js
+++ vue-udemy/17-routing/src/main.js
@@ -5,13 +5,21 @@ import App from './App.vue';
 import TeamsList from '@/components/teams/TeamsList.vue';
 import UsersList from '@/components/users/UsersList.vue';
 import TeamMembers from '@/components/teams/TeamMembers.vue';
+import NotFound from '@/components/nav/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    /* URL이 변함  */
+    { path: '/', redirect: '/teams' },
+    /* alias 이용시 URL이 변하지 않음 */
+    // { path: '/teams', component: TeamsList, alias: '/' },
     { path: '/teams', component: TeamsList },
     { path: '/users', component: UsersList },
     { path: '/teams/:teamId', component: TeamMembers, props: true },
+    /* Catch All 라우트 */
+    // { path: '/:notFound(.*)', redirect: '/teams' },
+    { path: '/:notFound(.*)', component: NotFound },
   ],
   linkActiveClass: 'active',
 });
