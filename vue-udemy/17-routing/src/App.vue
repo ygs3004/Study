9cdb17a610db8a233263da9801abc8c51139bceb
윤건수 <ygs3004@naver.com>
2023-05-29 20:19:23 +0900

    [2023/05/29] udemy - Vue 170강  - 섹션13: 라우팅(Routing): "다중 페이지"가 있는 SPA 구축하기   > 170강: router-link로 탐색하기

--- vue-udemy/17-routing/src/App.vue
+++ vue-udemy/17-routing/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <the-navigation @set-page='setActivePage'></the-navigation>
+  <the-navigation></the-navigation>
   <main>
     <!--    <component :is="activePage"></component>-->
     <router-view></router-view>
@@ -19,7 +19,6 @@ export default {
   },
   data() {
     return {
-      activePage: 'teams-list',
       teams: [
         { id: 't1', name: 'Frontend Engineers', members: ['u1', 'u2'] },
         { id: 't2', name: 'Backend Engineers', members: ['u1', 'u2', 'u3'] },
@@ -39,11 +38,6 @@ export default {
       teams: this.teams,
       users: this.users
     };
-  },
-  methods: {
-    setActivePage(page) {
-      this.activePage = page;
-    }
   }
 };
 </script>
