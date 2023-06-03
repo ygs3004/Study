665e7e78bd5a4e8648f9c83ae8e234388eaa14db
윤건수 <ygs3004@naver.com>
2023-06-03 22:00:15 +0900

    [2023/06/03] udemy - Vue 187강  - 섹션13: 라우팅(Routing): "다중 페이지"가 있는 SPA 구축하기   > 187강: 라우트 Leave 가드

--- vue-udemy/17-routing/src/components/users/UsersList.vue
+++ vue-udemy/17-routing/src/components/users/UsersList.vue
@@ -1,5 +1,6 @@
 <template>
   <button @click='confirmInput'>Confirm</button>
+  <button @click='saveChanges'>Save Changes</button>
   <ul>
     <user-item v-for='user in users' :key='user.id' :name='user.fullName' :role='user.role'></user-item>
   </ul>
@@ -13,6 +14,11 @@ export default {
     UserItem
   },
   inject: ['users'],
+  dat() {
+    return {
+      changesSaved: false
+    };
+  },
   methods: {
     confirmInput() {
       this.$router.push('/teams');
@@ -20,12 +26,30 @@ export default {
       // this.$router.back();
       // 앞으로가기
       // this.$router.forward();
+    },
+    saveChanges() {
+      this.changesSaved = true;
     }
+
   },
   beforeRouteEnter(to, from, next) {
     console.log('UserList Cmp beforeRouteEnter');
     console.log(to, from);
     next();
+  },
+  beforeRouteLeave(to, from, next) {
+    console.log('UserList Cmp beforeRouteLeave');
+    console.log(to, from);
+    if (this.changesSaved) {
+      next();
+    } else {
+      const userWantsToLeave = confirm('Are you sure? You get unsaved changes!');
+      next(userWantsToLeave);
+    }
+
+  },
+  unmounted() {
+    console.log('unmounted');
   }
 };
 </script>
