4e60dac705cec6d33ca76ace1aec0a3a906f9efb
윤건수 <ygs3004@naver.com>
2023-06-04 02:23:14 +0900

    [2023/06/04] udemy - Vue 199강 - 섹션14: 애니메이션 및 전환   > 199강: 커스턴 CSS 클래스 이름 사용하기

--- vue-udemy/18-animations/src/App.vue
+++ vue-udemy/18-animations/src/App.vue
@@ -9,7 +9,7 @@
     <!-- Element mounted -->
     <!-- -leave-from, -leave-active, -leave-to   -->
     <!-- Element not mounted -->
-    <transition>
+    <transition name='para'>
       <p v-if='paraIsVisible'>버튼을 눌러야 보입니다!</p>
     </transition>
     <button @click='toggleParagraph'>문단 열기</button>
@@ -98,32 +98,32 @@ button:active {
   border-radius: 12px;
 }
 
-.v-enter-from {
+.para-enter-from {
   /*opacity: 0;*/
   /*transform: translateY(-30px);*/
 }
 
-.v-enter-active {
+.para-enter-active {
   /*transition: all 0.3s ease-out;*/
   animation: slide-scale 0.3s ease-out
 }
 
-.v-enter-to {
+.para-enter-to {
   /*opacity: 1;*/
   /*transform: translateY(0);*/
 }
 
-.v-leave-from {
+.para-leave-from {
   /*opacity: 0.5;*/
   /*transform: translateY(0);*/
 }
 
-.v-leave-active {
+.para-leave-active {
   /*transition: all 0.3s ease-in;*/
   animation: slide-scale 0.3s ease-out;
 }
 
-.v-leave-to {
+.para-leave-to {
   /*opacity: 0;*/
   /*transform: translateY(100px);*/
 }
