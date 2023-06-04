f9b0c204800c8104cddf6b06b06ad49db6eaaa2a
윤건수 <ygs3004@naver.com>
2023-06-05 00:53:42 +0900

    [2023/06/04] udemy - Vue 202강 - 섹션14: 애니메이션 및 전환   > 202강: 전환 이벤트 사용하기

--- vue-udemy/18-animations/src/App.vue
+++ vue-udemy/18-animations/src/App.vue
@@ -9,7 +9,14 @@
     <!-- Element mounted -->
     <!-- -leave-from, -leave-active, -leave-to   -->
     <!-- Element not mounted -->
-    <transition name='para'>
+    <transition name='para'
+                @before-enter='beforeEnter'
+                @enter='enter'
+                @after-enter='afterEnter'
+                @before-leave='beforeLeave'
+                @leave='leave'
+                @after-leave='afterLeave'
+    >
       <p v-if='paraIsVisible'>버튼을 눌러야 보입니다!</p>
     </transition>
     <button @click='toggleParagraph'>문단 열기</button>
@@ -40,6 +47,30 @@ export default {
     };
   },
   methods: {
+    beforeEnter(el) {
+      console.log('beforeEnter');
+      console.log(el);
+    },
+    enter(el) {
+      console.log('enter');
+      console.log(el);
+    },
+    afterEnter(el) {
+      console.log('afterEnter');
+      console.log(el);
+    },
+    beforeLeave(el) {
+      console.log('beforeLeave');
+      console.log(el);
+    },
+    leave(el) {
+      console.log('leave');
+      console.log(el);
+    },
+    afterLeave(el) {
+      console.log('afterLeave');
+      console.log(el);
+    },
     animateBlock() {
       this.animatedBlock = true;
     },
