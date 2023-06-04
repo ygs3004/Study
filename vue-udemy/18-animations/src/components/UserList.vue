e342333592adede6216346483bcd953ecbc07319
윤건수 <ygs3004@naver.com>
2023-06-05 01:50:30 +0900

    [2023/06/05] udemy - Vue 207강 - 섹션14: 애니메이션 및 전환   > 207강: 목록 항목 이동에 애니메이션 적용하기

--- vue-udemy/18-animations/src/components/UserList.vue
+++ vue-udemy/18-animations/src/components/UserList.vue
@@ -57,10 +57,15 @@ li {
 
 .user-list-leave-active {
   transition: all 1s ease-in;
+  position: absolute
 }
 
 .user-list-leave-to {
   opacity: 0;
   transform: translateX(30px);
+}
+
+.user-list-move {
+  transition: transform 0.8s ease
 }
 </style>
