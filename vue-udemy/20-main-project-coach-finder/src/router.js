d9920691b0fcb9b2124df8df1059fcff2071be05
윤건수 <ygs3004@naver.com>
2023-06-06 04:15:10 +0900

    [2023/06/06] udemy - Vue 237강 - section 16: Main Project - Coach Finder   > 237강: 라우트 페이지 컴포넌트 추가하기

--- vue-udemy/20-main-project-coach-finder/src/router.js
+++ vue-udemy/20-main-project-coach-finder/src/router.js
@@ -1,19 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import CoachesList from '@/pages/coaches/CoachesList.vue';
+import CoachDetails from '@/pages/coaches/CoachDetails.vue';
+import CoachRegistration from '@/pages/coaches/CoachRegistration.vue';
+import ContactCoach from '@/pages/requests/ContactCoach.vue';
+import requestReceived from '@/pages/requests/RequestReceived.vue';
+import NotFound from '@/pages/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches' },
-    { path: '/coaches', component: null },
+    { path: '/coaches', component: CoachesList },
     {
       path: '/coaches/:id',
-      component: null,
-      children: [{ path: 'contact', component: null }],
+      component: CoachDetails,
+      children: [{ path: 'contact', component: ContactCoach }],
     },
-    { path: 'register', component: null },
-    { path: '/coaches/:id', component: null },
-    { path: '/requests', component: null },
-    { path: '/:notFount(.*)', component: null },
+    { path: 'register', component: CoachRegistration },
+    { path: '/requests', component: requestReceived },
+    { path: '/:notFount(.*)', component: NotFound },
   ],
 });
 
