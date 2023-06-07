8cf822ae4d79f96bc0bc6cfc128d1c2f200d9803
윤건수 <ygs3004@naver.com>
2023-06-07 23:23:08 +0900

    [2023/06/07] udemy - Vue 241강 - section 16: Main Project - Coach Finder   > 241강: CoachesList 및 CoachItem 작성

--- vue-udemy/20-main-project-coach-finder/src/pages/coaches/CoachesList.vue
+++ vue-udemy/20-main-project-coach-finder/src/pages/coaches/CoachesList.vue
@@ -8,16 +8,25 @@
       <router-link to='/register'>Register as Coach</router-link>
     </div>
     <ul v-if='hasCoaches'>
-      <li v-for='coach in filteredCoaches' :key='coach.id'>
-        {{ coach.firstName }}
-      </li>
+      <coach-item
+        v-for='coach in filteredCoaches'
+        :key='coach.id'
+        :id='coach.id'
+        :first-name='coach.firstName'
+        :last-name='coach.lastName'
+        :rate='coach.hourlyRate'
+        :areas='coach.areas'
+      ></coach-item>
     </ul>
     <h3 v-else>No Coaches Found.</h3>
   </section>
 </template>
 
 <script>
+import CoachItem from '@/components/coaches/CoachItem.vue';
+
 export default {
+  components: { CoachItem },
   computed: {
     filteredCoaches() {
       return this.$store.getters['coaches/coaches'];
@@ -30,5 +39,14 @@ export default {
 </script>
 
 <style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
 
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
 </style>
