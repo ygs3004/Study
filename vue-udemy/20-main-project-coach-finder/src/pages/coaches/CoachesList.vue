482e76bf419b518831925dcfae5efc074792448c
윤건수 <ygs3004@naver.com>
2023-06-07 23:06:44 +0900

    [2023/06/07] udemy - Vue 240강 - section 16: Main Project - Coach Finder   > 240강: Vuex 및 코치 데이터 추가하기

--- vue-udemy/20-main-project-coach-finder/src/pages/coaches/CoachesList.vue
+++ vue-udemy/20-main-project-coach-finder/src/pages/coaches/CoachesList.vue
@@ -7,14 +7,26 @@
       <button>Refresh</button>
       <router-link to='/register'>Register as Coach</router-link>
     </div>
-    <ul>
-      List of Coach
+    <ul v-if='hasCoaches'>
+      <li v-for='coach in filteredCoaches' :key='coach.id'>
+        {{ coach.firstName }}
+      </li>
     </ul>
+    <h3 v-else>No Coaches Found.</h3>
   </section>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    }
+  }
+};
 </script>
 
 <style scoped>
