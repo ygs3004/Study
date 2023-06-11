8ef62f4f366a47c3609bc649e8bb02e2ec0e43e3
윤건수 <ygs3004@naver.com>
2023-06-11 23:01:55 +0900

    [2023/06/11] udemy - Vue 250강 - section 16: Main Project - Coach Finder   > 250강: message 창 구현

--- vue-udemy/20-main-project-coach-finder/src/pages/requests/RequestReceived.vue
+++ vue-udemy/20-main-project-coach-finder/src/pages/requests/RequestReceived.vue
@@ -1,10 +1,51 @@
 <template>
-  Requests
+  <section>
+    <base-card>
+      <header>
+        <h2>Requests Received</h2>
+      </header>
+      <ul v-if="hasRequests">
+        <requests-item
+          v-for="req in receivedRequests"
+          :key="req.id"
+          :email="req.userEmail"
+          :message="req.message"
+        ></requests-item>
+      </ul>
+      <h3 v-else>You haven't received any requests yet!</h3>
+    </base-card>
+  </section>
 </template>
+
 <script>
-export default {};
+import RequestsItem from '@/pages/requests/RequestsItem.vue';
+
+export default {
+  components: { RequestsItem },
+  computed: {
+    receivedRequests() {
+      return this.$store.getters['requests/requests'];
+    },
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests'];
+    },
+  },
+};
 </script>
 
 <style scoped>
+header {
+  text-align: center;
+}
 
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
 </style>
--- vue-udemy/20-main-project-coach-finder/src/store/modules/requests/index.js
+++ vue-udemy/20-main-project-coach-finder/src/store/modules/requests/index.js
@@ -1,5 +1,6 @@
 import mutations from '@/store/modules/requests/mutations.js';
 import actions from '@/store/modules/requests/actions.js';
+import getters from '@/store/modules/requests/getters.js';
 
 export default {
   namespaced: true,
@@ -10,4 +11,5 @@ export default {
   },
   mutations,
   actions,
+  getters,
 };
