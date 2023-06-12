e6a5f439aa0889bdf6f55762e035c98615160840
윤건수 <ygs3004@naver.com>
2023-06-12 23:44:44 +0900

    [2023/06/12] udemy - Vue 253강 - section 16: Main Project - Coach Finder   > 253강: GET 요청 전송으로 데이터 불러오기

--- vue-udemy/20-main-project-coach-finder/src/pages/coaches/CoachesList.vue
+++ vue-udemy/20-main-project-coach-finder/src/pages/coaches/CoachesList.vue
@@ -5,7 +5,7 @@
   <section>
     <base-card>
       <div class="controls">
-        <base-button mode="outline">Refresh</base-button>
+        <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
         <base-button v-if="!isCoach" link to="/register"
           >Register as Coach
         </base-button>
@@ -64,9 +64,15 @@ export default {
       return this.$store.getters['coaches/hasCoaches'];
     },
   },
+  created() {
+    this.loadCoaches();
+  },
   methods: {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
+    },
+    loadCoaches() {
+      this.$store.dispatch('coaches/loadCoaches');
     },
   },
 };
--- vue-udemy/20-main-project-coach-finder/src/store/modules/coaches/actions.js
+++ vue-udemy/20-main-project-coach-finder/src/store/modules/coaches/actions.js
@@ -10,16 +10,6 @@ export default {
       areas: data.areas,
     };
 
-    // const response = await fetch(
-    //   `https://coach-finder-91adb-default-rtdb.firebaseio.com/coaches/${userId}.json`,
-    //   {
-    //     method: 'PUT',
-    //     body: JSON.stringify(coachData),
-    //   }
-    // );
-    //
-    // const responseData = await response.json();
-
     const response = await fetch(
       `https://coach-finder-91adb-default-rtdb.firebaseio.com/coaches/${userId}.json`,
       {
@@ -40,5 +30,31 @@ export default {
         ...coachData,
         id: userId,
       });
+  },
+
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://coach-finder-91adb-default-rtdb.firebaseio.com/coaches.json`
+    );
+
+    const responseData = await response.json();
+    if (!response.ok) {
+      // ...error
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+    context.commit('setCoaches', coaches);
   },
 };
