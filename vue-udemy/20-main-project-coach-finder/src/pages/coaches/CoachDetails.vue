591db140e710348d3f4617f4d068d44caa1c2525
윤건수 <ygs3004@naver.com>
2023-06-12 23:18:21 +0900

    [2023/06/12] udemy - Vue 252강 - section 16: Main Project - Coach Finder   > 252강: PUT 요청 전송으로 데이터 저장

--- vue-udemy/20-main-project-coach-finder/src/pages/coaches/CoachDetails.vue
+++ vue-udemy/20-main-project-coach-finder/src/pages/coaches/CoachDetails.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <base-card>
-      <h2>{{ fullName }}}</h2>
+      <h2>{{ fullName }}</h2>
       <h3>${{ rate }}/hour</h3>
     </base-card>
   </section>
--- vue-udemy/20-main-project-coach-finder/src/store/modules/coaches/actions.js
+++ vue-udemy/20-main-project-coach-finder/src/store/modules/coaches/actions.js
@@ -1,5 +1,6 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
       id: context.rootGetters.userId,
       firstName: data.first,
@@ -9,6 +10,35 @@ export default {
       areas: data.areas,
     };
 
-    context.commit('registerCoach', coachData);
+    // const response = await fetch(
+    //   `https://coach-finder-91adb-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+    //   {
+    //     method: 'PUT',
+    //     body: JSON.stringify(coachData),
+    //   }
+    // );
+    //
+    // const responseData = await response.json();
+
+    const response = await fetch(
+      `https://coach-finder-91adb-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      }
+    );
+
+    const responseData = await response.json();
+    console.log(responseData);
+
+    if (!response.ok) {
+      // error...
+    }
+
+    if (response)
+      context.commit('registerCoach', {
+        ...coachData,
+        id: userId,
+      });
   },
 };
