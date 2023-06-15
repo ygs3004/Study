222cd7d7759a753e336b004138fd3ea8044b2db2
윤건수 <ygs3004@naver.com>
2023-06-15 23:03:44 +0900

    [2023/06/15] udemy - Vue 259, 260강 - section 16: Main Project - Coach Finder   > 259강: Not Found Page   > 260강: Section 자료

--- vue-udemy/20-main-project-coach-finder/src/pages/NotFound.vue
+++ vue-udemy/20-main-project-coach-finder/src/pages/NotFound.vue
@@ -1,7 +1,12 @@
-<script>
-export default {};
-</script>
-
-<style scoped>
-
-</style>
+<template>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        this page could not be found - maybe check out all our
+        <router-link to="/coaches">coaches.</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
+<script setup></script>
