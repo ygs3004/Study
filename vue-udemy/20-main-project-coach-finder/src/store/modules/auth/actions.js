9395a25afd0f96723146e02d6b0accfc5f60b48c
윤건수 <ygs3004@naver.com>
2023-06-17 21:53:42 +0900

    [2023/06/17] udemy - Vue 273강 - section 17: Vue 인증   > 273강: 자동 로그인 추가하기

--- vue-udemy/20-main-project-coach-finder/src/store/modules/auth/actions.js
+++ vue-udemy/20-main-project-coach-finder/src/store/modules/auth/actions.js
@@ -1,59 +1,60 @@
 export default {
   async login(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDTBXRsNzsjnd8CtvHK3IOD-Hghdu-HiW8',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
-
-    const responseData = await response.json();
-    if (!response.ok) {
-      console.log(responseData);
-      throw new Error(
-        responseData.message || 'Failed to authenticate. Check your login data'
-      );
-    }
-    console.log(responseData);
-    context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresId,
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login',
     });
   },
   async signup(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDTBXRsNzsjnd8CtvHK3IOD-Hghdu-HiW8',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup',
+    });
+  },
+  async auth(context, payload) {
+    const mode = payload.mode;
+    const url =
+      mode === 'login'
+        ? 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDTBXRsNzsjnd8CtvHK3IOD-Hghdu-HiW8'
+        : 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDTBXRsNzsjnd8CtvHK3IOD-Hghdu-HiW8';
+
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
+    });
 
     const responseData = await response.json();
     if (!response.ok) {
-      console.log(responseData);
       throw new Error(
         responseData.message || 'Failed to authenticate. Check your login data'
       );
     }
-    console.log(responseData);
+
+    localStorage.setItem('token', responseData.idToken);
+    localStorage.setItem('userId', responseData.localId);
+
     context.commit('setUser', {
       token: responseData.idToken,
       userId: responseData.localId,
       tokenExpiration: responseData.expiresId,
     });
   },
+  tryLogin(context) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
 
+    if (token && userId) {
+      context.commit('setUser', {
+        token,
+        userId,
+        tokenExpiration: null,
+      });
+    }
+  },
   logout(context) {
     context.commit('setUser', {
       tokenExpiration: null,
