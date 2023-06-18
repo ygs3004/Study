e4c8144bb9346342827cf09a4ec7faa6f3e963d8
윤건수 <ygs3004@naver.com>
2023-06-18 23:04:09 +0900

    [2023/06/18] udemy - Vue 291강 - section 19: 컴포지션 API(옵션 API 대체하기)   > 291강: 실습 과제(데이터 및 함수)

---
+++ vue-udemy/composition-assignment-1-problem/src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <h2>My Course Goal</h2>
+
+  <!-- Separate Refs -->
+  <!--  <h3 v-if="showGoal">{{ courseGoal }}</h3>-->
+
+  <!-- Ref Object -->
+  <!-- <h3 v-if="goal.showGoal">{{ goal.courseGoal }}</h3>-->
+
+  <!-- Reactive Object -->
+  <!--  <h3 v-if="goal.showGoal">{{ goal.courseGoal }}</h3>-->
+
+  <!--Options API-->
+  <h3 v-if="showGoal">{{ courseGoal }}</h3>
+
+  <button @click="toggleGoal">Toggle Goal</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      courseGoal: 'becoming a vue of master',
+      showGoal: true,
+    };
+  },
+  methods: {
+    toggleGoal() {
+      this.showGoal = !this.showGoal;
+    },
+  },
+
+  // setup() {
+  // Separate refs
+  // const courseGoal = ref('becoming a vue of master');
+  // const showGoal = ref(true);
+  // const toggleGoal = () => {
+  //   showGoal.value = !showGoal.value;
+  // };
+  // Ref Object
+  // const goal = ref({
+  //   courseGoal: 'becoming a vue of master',
+  //   showGoal: true,
+  // });
+  // const toggleGoal = () => {
+  //   goal.value.showGoal = !goal.value.showGoal;
+  // };
+  // Reactive Object
+  // const goal = reactive({
+  //   courseGoal: 'becoming a vue of master',
+  //   showGoal: true,
+  // });
+  //
+  // const toggleGoal = () => {
+  //   goal.showGoal = !goal.showGoal;
+  // };
+  // return {
+  // Separate refs
+  // courseGoal,
+  // showGoal,
+  // toggleGoal,
+  //
+  // Ref Object
+  // goal,
+  // toggleGoal,
+  // Reactive Object
+  //   goal,
+  //   toggleGoal,
+  // };
+  // },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 3rem;
+  text-align: center;
+}
+</style>
