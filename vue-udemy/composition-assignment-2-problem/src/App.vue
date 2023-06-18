ecde97af95b73d2d169d0da4c6d0f75cb28c7639
윤건수 <ygs3004@naver.com>
2023-06-19 02:49:30 +0900

    [2023/06/19] udemy - Vue 과제 8 실습시간 - section 19: 컴포지션 API(옵션 API 대체하기)   > 과제8 실습시간: 컴포지션 API 핵심 구성 요소

--- vue-udemy/composition-assignment-2-problem/src/App.vue
+++ vue-udemy/composition-assignment-2-problem/src/App.vue
@@ -19,33 +19,50 @@
   </section>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      availableFunds: 100,
-      currentExpenses: 0,
-      enteredExpense: 0,
-    };
-  },
-  computed: {
-    remainingFunds() {
-      return this.availableFunds - this.currentExpenses;
-    },
-  },
-  methods: {
-    addExpense() {
-      this.currentExpenses += this.enteredExpense;
-    },
-  },
-  watch: {
-    remainingFunds(val) {
-      if (val < 0) {
-        alert('You are broke!');
-      }
-    },
-  },
-};
+<script setup>
+import { computed, ref, watch } from 'vue';
+
+const availableFunds = 100;
+const currentExpenses = ref(0);
+const enteredExpense = ref(0);
+
+const addExpense =  () => {
+  currentExpenses.value += enteredExpense.value;
+}
+
+const remainingFunds = computed(() =>  availableFunds - currentExpenses.value);
+watch(remainingFunds, (val) => {
+         if (val < 0) {
+         alert('You are broke!');
+       }
+});
+
+// export default {
+//   data() {
+//     return {
+//       availableFunds: 100,
+//       currentExpenses: 0,
+//       enteredExpense: 0,
+//     };
+//   },
+//   computed: {
+//     remainingFunds() {
+//       return this.availableFunds - this.currentExpenses;
+//     },
+//   },
+//   methods: {
+//     addExpense() {
+//       this.currentExpenses += this.enteredExpense;
+//     },
+//   },
+//   watch: {
+//     remainingFunds(val) {
+//       if (val < 0) {
+//         alert('You are broke!');
+//       }
+//     },
+//   },
+// };
 </script>
 
 <style>
