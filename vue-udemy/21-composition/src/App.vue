d0d18724262feaad2220268e8ff840515332d7f8
윤건수 <ygs3004@naver.com>
2023-06-18 21:53:51 +0900

    [2023/06/18] udemy - Vue 289강 - section 19: 컴포지션 API(옵션 API 대체하기)   > 289강: 반응성: 심층 분석

--- vue-udemy/21-composition/src/App.vue
+++ vue-udemy/21-composition/src/App.vue
@@ -2,30 +2,38 @@
   <section class="container">
     <!--    <h2>{{ userName }}</h2>-->
     <!--    <h3>{{ age }}</h3>-->
-    <h2>{{ user.name }}</h2>
-    <h3>{{ user.age }}</h3>
+    <h2>{{ name }}</h2>
+    <h2>{{ age }}</h2>
   </section>
 </template>
 
-<script setup>
-// import { ref } from 'vue';
+<script>
+import { isReactive, isRef, reactive, ref, toRefs } from 'vue';
 
-import { reactive } from 'vue';
+export default {
+  setup() {
+    const user = reactive({
+      name: 'Ygs',
+      age: 20,
+    });
+    const uAge = ref(31);
 
-const user = reactive({
-  name: 'Ygs',
-  age: 20,
-});
+    console.log(isRef(uAge));
+    console.log(isReactive(user));
+    const userRefs = toRefs(user);
 
-// const userName = ref('Maximilian');
-// const age = ref(31);
+    setTimeout(() => {
+      user.name = 'Yoon Gun Soo';
+      user.age = 21;
+    }, 2000);
 
-setTimeout(() => {
-  // userName.value = 'Max';
-  // age.value = 30;
-  user.name = 'Yoon Gun SOo';
-  user.age = 21;
-}, 2000);
+    return {
+      user: user,
+      name: userRefs.name,
+      age: userRefs.age,
+    };
+  },
+};
 </script>
 
 <style>
