0b2ff40a197ef80e2974f80dad7643a1b8a01d67
윤건수 <ygs3004@naver.com>
2023-06-21 01:41:05 +0900

    [2023/06/21] udemy - Vue 301강 - section 19: 컴포지션 API(옵션 API 대체하기)   > 302강: 컴포넌트 이전하기(2)

--- vue-udemy/22-composition-2/src/components/users/UserList.vue
+++ vue-udemy/22-composition-2/src/components/users/UserList.vue
@@ -1,10 +1,17 @@
 <template>
   <base-container>
     <h2>Active Users</h2>
-    <base-search @search="updateSearch" :search-term="enteredSearchTerm"></base-search>
+    <base-search
+      @search="updateSearch"
+      :search-term="enteredSearchTerm"
+    ></base-search>
     <div>
-      <button @click="sort('asc')" :class="{selected: sorting === 'asc'}">Sort Ascending</button>
-      <button @click="sort('desc')" :class="{selected: sorting === 'desc'}">Sort Descending</button>
+      <button @click="sort('asc')" :class="{ selected: sorting === 'asc' }">
+        Sort Ascending
+      </button>
+      <button @click="sort('desc')" :class="{ selected: sorting === 'desc' }">
+        Sort Descending
+      </button>
     </div>
     <ul>
       <user-item
@@ -20,65 +27,124 @@
 
 <script>
 import UserItem from './UserItem.vue';
+import { computed, ref, watch } from 'vue';
 
 export default {
   components: {
     UserItem,
   },
   props: ['users'],
-  data() {
-    return {
-      enteredSearchTerm: '',
-      activeSearchTerm: '',
-      sorting: null,
-    };
-  },
-  computed: {
-    availableUsers() {
+  emits: ['list-projects'],
+  setup(props) {
+    const enteredSearchTerm = ref('');
+    const activeSearchTerm = ref('');
+    const availableUsers = computed(() => {
       let users = [];
-      if (this.activeSearchTerm) {
-        users = this.users.filter((usr) =>
-          usr.fullName.includes(this.activeSearchTerm)
+      if (activeSearchTerm.value) {
+        users = props.users.filter((usr) =>
+          usr.fullName.includes(activeSearchTerm.value)
         );
-      } else if (this.users) {
-        users = this.users;
+      } else if (props.users) {
+        users = props.users;
       }
       return users;
-    },
-    displayedUsers() {
-      if (!this.sorting) {
-        return this.availableUsers;
+    });
+    watch(enteredSearchTerm, (newValue) => {
+      setTimeout(() => {
+        if (newValue === enteredSearchTerm.value) {
+          activeSearchTerm.value = newValue;
+        }
+      }, 300);
+    });
+    const updateSearch = (val) => {
+      enteredSearchTerm.value = val;
+    };
+
+    const sorting = ref(null);
+    const displayedUsers = computed(() => {
+      if (!sorting.value) {
+        return availableUsers.value;
       }
-      return this.availableUsers.slice().sort((u1, u2) => {
-        if (this.sorting === 'asc' && u1.fullName > u2.fullName) {
+      return availableUsers.value.slice().sort((u1, u2) => {
+        if (sorting.value === 'asc' && u1.fullName > u2.fullName) {
           return 1;
-        } else if (this.sorting === 'asc') {
+        } else if (sorting.value === 'asc') {
           return -1;
-        } else if (this.sorting === 'desc' && u1.fullName > u2.fullName) {
+        } else if (sorting.value === 'desc' && u1.fullName > u2.fullName) {
           return -1;
         } else {
           return 1;
         }
       });
-    },
+    });
+    const sort = (mode) => {
+      sorting.value = mode;
+    };
+
+    return {
+      enteredSearchTerm,
+      activeSearchTerm,
+      sorting,
+      availableUsers,
+      displayedUsers,
+      updateSearch,
+      sort,
+    };
   },
-  methods: {
-    updateSearch(val) {
-      this.enteredSearchTerm = val;
-    },
-    sort(mode) {
-      this.sorting = mode;
-    },
-  },
-  watch: {
-    enteredSearchTerm(val) {
-      setTimeout(() => {
-        if (val === this.enteredSearchTerm) {
-          this.activeSearchTerm = val;
-        }
-      }, 300);
-    }
-  },
+
+  // data() {
+  //   return {
+  //     enteredSearchTerm: '',
+  //     activeSearchTerm: '',
+  //     sorting: null,
+  //   };
+  // },
+  // computed: {
+  //   availableUsers() {
+  //     let users = [];
+  //     if (this.activeSearchTerm) {
+  //       users = this.users.filter((usr) =>
+  //         usr.fullName.includes(this.activeSearchTerm)
+  //       );
+  //     } else if (this.users) {
+  //       users = this.users;
+  //     }
+  //     return users;
+  //   },
+  //   displayedUsers() {
+  //     if (!this.sorting) {
+  //       return this.availableUsers;
+  //     }
+  //     return this.availableUsers.slice().sort((u1, u2) => {
+  //       if (this.sorting === 'asc' && u1.fullName > u2.fullName) {
+  //         return 1;
+  //       } else if (this.sorting === 'asc') {
+  //         return -1;
+  //       } else if (this.sorting === 'desc' && u1.fullName > u2.fullName) {
+  //         return -1;
+  //       } else {
+  //         return 1;
+  //       }
+  //     });
+  //   },
+  // },
+  // methods: {
+  //   updateSearch(val) {
+  //     this.enteredSearchTerm = val;
+  //   },
+  //   sort(mode) {
+  //     this.sorting = mode;
+  //   },
+  // },
+  // watch: {
+  //   enteredSearchTerm(val) {
+  //     setTimeout(() => {
+  //       if (val === this.enteredSearchTerm) {
+  //         this.activeSearchTerm = val;
+  //       }
+  //     }, 300);
+  //   },
+  // },
 };
 </script>
 
