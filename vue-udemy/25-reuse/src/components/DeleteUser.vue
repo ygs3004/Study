d7eb438b6b34163bbaf4d2f77d3448cabad86dc6
윤건수 <ygs3004@naver.com>
2023-06-22 01:08:37 +0900

    [2023/06/22] udemy - Vue 312,313rkd강 - section 20: 기능 재사용: 믹스인(Mixin) 및 커스텀 컴포지션 함수   > 312강: 믹스인 병합 이해하기   > 313강: 전역 믹스인

--- vue-udemy/25-reuse/src/components/DeleteUser.vue
+++ vue-udemy/25-reuse/src/components/DeleteUser.vue
@@ -1,5 +1,5 @@
 <template>
-  <user-alert v-if="alertIsVisible" title="Delete the User?" @close="hideAlert">
+  <user-alert v-if="alertIsVisible" :title="alertTitle" @close="hideAlert">
     <p>Do you want to continue with deleting a user?</p>
   </user-alert>
   <section>
@@ -15,6 +15,12 @@ import UserAlert from '@/components/UserAlert.vue';
 export default {
   components: {
     UserAlert,
+  },
+  data(){
+    return{
+      alertTitle: 'Delete the User?',
+      alertIsVisible: true,
+    }
   },
   mixins: [alertMixin],
 }
--- vue-udemy/25-reuse/src/main.js
+++ vue-udemy/25-reuse/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
+import loggerMixing from './mixins/laogger.js'
 
 const app = createApp(App)
 
+app.mixin(loggerMixing)
 app.mount('#app');
