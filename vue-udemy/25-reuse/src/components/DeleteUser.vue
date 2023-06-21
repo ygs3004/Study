9529954ebb6a376f93cd549bf15dff4c1ab19b6f
윤건수 <ygs3004@naver.com>
2023-06-22 00:12:40 +0900

    [2023/06/22] udemy - Vue 307, 308강 - section 19: 컴포지션 API(옵션 API 대체하기)   > 307강: 요약   > 308강: 섹션 자료 - section 20: 기능 재사용: 믹스인(Mixin) 및 커스텀 컴포지션 함수   > 309강: 섹션 소개   > 310강: 재사용성 개념   > 311강: 믹스인(Mixin) 사용하기

---
+++ vue-udemy/25-reuse/src/components/DeleteUser.vue
@@ -0,0 +1,21 @@
+<template>
+  <user-alert v-if="alertIsVisible" title="Delete the User?" @close="hideAlert">
+    <p>Do you want to continue with deleting a user?</p>
+  </user-alert>
+  <section>
+    <h2>Delete a User</h2>
+    <button @click="showAlert">Delete User</button>
+  </section>
+</template>
+
+<script>
+import alertMixin from '../mixins/alert.js'
+import UserAlert from '@/components/UserAlert.vue';
+
+export default {
+  components: {
+    UserAlert,
+  },
+  mixins: [alertMixin],
+}
+</script>
---
+++ vue-udemy/25-reuse/src/mixins/alert.js
@@ -0,0 +1,17 @@
+
+export default {
+
+  data() {
+    return {
+      alertIsVisible: false,
+    };
+  },
+  methods: {
+    showAlert() {
+      this.alertIsVisible = true;
+    },
+    hideAlert() {
+      this.alertIsVisible = false;
+    },
+  },
+};
