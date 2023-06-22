de7c7c0e4ee62d4e6504cd5682667fd32ef92aaf
윤건수 <ygs3004@naver.com>
2023-06-23 02:25:32 +0900

    [2023/06/23] udemy - Vue 321강 - section 20: 기능 재사용: 믹스인(Mixin) 및 커스텀 컴포지션 함수   > 321강: 정렬(Sort) 커스텀 훅

--- vue-udemy/27-reuse/src/components/users/UserList.vue
+++ vue-udemy/27-reuse/src/components/users/UserList.vue
@@ -26,10 +26,11 @@
 </template>
 
 <script>
-import { computed, ref, toRefs } from 'vue';
+import { toRefs } from 'vue';
 
 import UserItem from './UserItem.vue';
 import useSearch from '@/hoooks/search';
+import useSort from '@/hoooks/sort';
 
 export default {
   components: {
@@ -46,27 +47,10 @@ export default {
       updateSearch,
     } = useSearch(users, 'fullName');
 
-    const sorting = ref(null);
-    const displayedUsers = computed(function () {
-      if (!sorting.value) {
-        return availableUsers.value;
-      }
-      return availableUsers.value.slice().sort((u1, u2) => {
-        if (sorting.value === 'asc' && u1.fullName > u2.fullName) {
-          return 1;
-        } else if (sorting.value === 'asc') {
-          return -1;
-        } else if (sorting.value === 'desc' && u1.fullName > u2.fullName) {
-          return -1;
-        } else {
-          return 1;
-        }
-      });
-    });
-
-    function sort(mode) {
-      sorting.value = mode;
-    }
+    const { sorting, displayedUsers, sort } = useSort(
+      availableUsers,
+      'fullName'
+    );
 
     return {
       enteredSearchTerm,
@@ -76,59 +60,6 @@ export default {
       sort,
     };
   },
-  // data() {
-  //   return {
-  //     enteredSearchTerm: '',
-  //     activeSearchTerm: '',
-  //     sorting: null,
-  //   };
-  // },
-  // computed: {
-  //   availableUsers() {
-  //     let users = [];
-  //     if (this.activeSearchTerm) {
-  //       users = this.users.filter((usr) =>
-  //         usr.fullName.includes(this.activeSearchTerm)
-  //       );
-  //     } else if (this.users) {
-  //       users = this.users;
-  //     }
-  //     return users;
-  //   },
-  //   displayedUsers() {
-  //     if (!this.sorting) {
-  //       return this.availableUsers;
-  //     }
-  //     return this.availableUsers.slice().sort((u1, u2) => {
-  //       if (this.sorting === 'asc' && u1.fullName > u2.fullName) {
-  //         return 1;
-  //       } else if (this.sorting === 'asc') {
-  //         return -1;
-  //       } else if (this.sorting === 'desc' && u1.fullName > u2.fullName) {
-  //         return -1;
-  //       } else {
-  //         return 1;
-  //       }
-  //     });
-  //   },
-  // },
-  // methods: {
-  //   updateSearch(val) {
-  //     this.enteredSearchTerm = val;
-  //   },
-  //   sort(mode) {
-  //     this.sorting = mode;
-  //   },
-  // },
-  // watch: {
-  //   enteredSearchTerm(val) {
-  //     setTimeout(() => {
-  //       if (val === this.enteredSearchTerm) {
-  //         this.activeSearchTerm = val;
-  //       }
-  //     }, 300);
-  //   }
-  // },
 };
 </script>
 
