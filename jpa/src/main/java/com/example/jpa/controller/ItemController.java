55133963c1d040bf78928a40d761e8a75cf49192
윤건수 <ygs3004@naver.com>
2024-03-17 18:23:19 +0900

    [2024/03/17] 자바 ORM 표준 JPA 프로그래밍  - Item Web

---
+++ jpa/src/main/java/com/example/jpa/controller/ItemController.java
@@ -0,0 +1,53 @@
+package com.example.jpa.controller;
+
+import com.example.jpa.model.entity.Book;
+import com.example.jpa.model.entity.Item;
+import com.example.jpa.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    /* 목록조회 */
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
+
+    /* 상품생성 */
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        return "/items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(Book item) {
+        itemService.saveItem(item);
+        return "redirect:/items";
+    }
+
+    /* 상품 수정 */
+    @GetMapping("/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Item item = itemService.findOne(itemId);
+        model.addAttribute("item", item);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("/items/{itemId}/edit")
+    public String updateItem(@ModelAttribute("item") Book item) {
+        itemService.saveItem(item);
+        return "redirect:/items";
+    }
+
+}
